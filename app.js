03cf8a7ab3379d0455c12ef26ec0dfd6d44d620d
arielwc <arielwalkercochrane@gmail.com>
2020-12-09 14:31:06 +1300

    Weather data coming in from API, displaying description, temp and image on page

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
-const { json } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  // API Key = 821f69af563f3ec45263448463fa1131
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.placeName;
+  const apiKey = "821f69af563f3ec45263448463fa1131";
+  const unit = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Katikati&appid=821f69af563f3ec45263448463fa1131";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+
   https.get(url, function (response) {
     console.log(response.statusCode);
+
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
-      console.log(weatherData.name);
+      const temps = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "<h1>The weather in " +
+          query +
+          " is now: " +
+          weatherDescription +
+          ".</h1>"
+      );
+      res.write(
+        "<h1>The temperature in " +
+          query +
+          " is now " +
+          temps +
+          " degrees Celcius.</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
     });
   });
-
-  res.send("");
 });
 
 app.listen(3000, function () {
